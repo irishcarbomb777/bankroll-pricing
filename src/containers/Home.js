a39c784aae0f6e9c8b02c1f2b9ef0a97c0959d07
irishcarbomb777 <danielclannan@gmail.com>
2020-12-08 10:30:16 -0500

    Setup routing and basic react

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Blueprint Pricing</h1>
+        <p className="text-muted">Create Pricing Sheet</p>
+      </div>
+    </div>
+  );
+}
